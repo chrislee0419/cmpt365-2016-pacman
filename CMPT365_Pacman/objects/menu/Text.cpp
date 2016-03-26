a739033da3a40c900e7de49648a7446d16670da5
chrislee0419 <cla235@sfu.ca>
2016-03-25 23:08:57 -0700

    using freetype for Text object

---
+++ CMPT365_Pacman/objects/menu/Text.cpp
@@ -0,0 +1,14 @@
+
+#include "Text.h"
+#include "..\..\test\test.h"
+#include "..\..\test\_util.h"
+
+using std::string;
+
+// Globals
+string _text;
+int _xpos;
+int _ypos;
+
+// Constructors
+Text::Text() {}
--- CMPT365_Pacman/objects/menu/Text.h
+++ CMPT365_Pacman/objects/menu/Text.h
@@ -6,18 +6,32 @@
 
 #include "..\..\depend\glew\glew.h"
 #include "..\..\depend\freeglut\freeglut.h"
+#include "..\..\depend\freetype\ft2build.h"
+#include "..\..\depend\freetype\freetype\freetype.h"
+#include "..\..\depend\freetype\freetype\ftglyph.h"
 
 class Text
 {
 private:
+	static FT_Library ft;
+	FT_Face face;
 	std::string _text;
-	void *_font;
+	int _xpos;
+	int _ypos;
 
 public:
 	Text();
-	Text(std::string text);
-	Text(void *font, std::string text);
+	Text(std::string text, int xpos, int ypos);
+	Text(void *font, std::string text, int xpos, int ypos);
+	~Text();
+
 	void SetFont(void *font);
 	void SetText(std::string text);
+	void SetXPosition(int xpos);
+	void SetYPosition(int ypos);
+	void SetPosition(int xpos, int ypos);
+
 	void Draw(int x_translate, int y_translate);
+
+	static void PrepareFontLibrary();
 };
