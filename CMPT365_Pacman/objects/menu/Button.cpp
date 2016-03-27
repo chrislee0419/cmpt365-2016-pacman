9d8b99715d007b0917a47a22b24e3f547290b20e
Matthew <tsengmatthew@hotmail.com>
2016-03-26 19:32:25 -0700

    starting Button, need to test and wait for Text

--- CMPT365_Pacman/objects/menu/Button.cpp
+++ CMPT365_Pacman/objects/menu/Button.cpp
@@ -1,11 +1,74 @@
 #include "Button.h"
 
-
+// Constructors
 Button::Button()
+{
+	box = Box();
+	// test = Test();
+}
+
+Button::Button(int xsize, int ysize, int xpos, int ypos, std::string text)
+{
+	box = Box(xsize, ysize, xpos, ypos);
+	// change text when Text class finished
+}
+
+// Destructor
+Button::~Button()
 {
 }
 
-
-Button::~Button()
+// Getter methods
+int Button::GetXSize()
 {
+	return box.GetXSize();
+}
+
+int Button::GetYSize()
+{
+	return box.GetYSize;
+}
+
+int Button::GetXPosition()
+{
+	return box.GetXPosition;
+}
+
+int Button::GetYPosition()
+{
+	return box.GetYPosition;
+}
+
+//Setter methods
+void Button::SetXSize(int x)
+{
+	box.SetXSize = x;
+}
+
+void Button::SetYSize(int y)
+{
+	box.SetYSize = y;
+}
+
+void Button::SetXPosition(int x)
+{
+	box.SetXPosition = x;
+}
+
+void Button::SetYPosition(int y)
+{
+	box.SetYPosition = y;
+}
+
+void Button::SetText(std::string text)
+{
+	// do something with text class
+}
+
+// checks mouse to see if over a button
+void Button::CheckMousePosition(int x, int y)
+{
+	if ((x < GetXPosition() + GetXSize() && x > GetXPosition())
+		&& (y < GetYPosition() + GetYSize() && y > GetYPosition()))
+		box.SetColour(BLACK, WHITE);
 }
--- CMPT365_Pacman/objects/menu/Button.h
+++ CMPT365_Pacman/objects/menu/Button.h
@@ -1,15 +1,39 @@
 #pragma once
 
 #include <iostream>
+#include <stdexcept>
+
+#include "..\..\depend\glew\glew.h"
+#include "..\..\depend\freeglut\freeglut.h"
+#include "..\..\depend\glm\vec4.hpp"
+#include "..\_colours.h"
+#include "Box.h"
+#include "Text.h"
 
 class Button
 {
 private:
-	int _xsize, _ysize, _xpos, _ypos;
+	Box box;
+	// Text text;
+	// callback function missing
 
 public:
 	Button();
 	Button(int xsize, int ysize, int xpos, int ypos, std::string text);
 	~Button();
+
+	int GetXSize();
+	int GetYSize();
+	int GetXPosition();
+	int GetYPosition();
+
+	void SetText(std::string text);
+	void SetXSize(int x);
+	void SetYSize(int y);
+	void SetXPosition(int x);
+	void SetYPosition(int y);
+
+	void CheckMousePosition(int x, int y);
+
 };
 
